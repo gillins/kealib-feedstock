e735795c2d30f388edef95c1697bee9b403f2550
Sam Gillingham <gillingham.sam@gmail.com>
2026-05-18 13:44:26 +1000

    WIP: do not merge. Test 2.0.0rc1

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,6 @@ cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
 
 make -j $CPU_COUNT
 
-#if [ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]; then
-#    ctest
-#fi
+if [ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]; then
+    ctest
+fi
